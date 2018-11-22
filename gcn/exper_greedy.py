604091fef42060156261f127b7321acbf6e920f6
floregol <florence.robert-regol@mail.mcgill.ca>
2018-11-22 14:56:04 -0500

    experiment

--- gcn/exper_greedy.py
+++ gcn/exper_greedy.py
@@ -90,49 +90,51 @@ def train_and_save_results(adj,
     if not os.path.exists(result_folder):
         os.makedirs(result_folder)
     trials = 5
+    K_sparse_list = [5,10,100]
+    noise_list = [0.01,1,100]
     for model_gcn in models_list:
-        result = []
-        K_sparse = 10
-        noise = 100
-        V_ksparse, V_ksparse_H, get_v, H, H_h, cov_x, cov_w, W, num_nodes = greedy_eigenvector_precomputation(
-            adj, K_sparse, noise, initial_train_mask)
+        for K_sparse in K_sparse_list:
+            for noise in noise_list:
+                result = []
+                V_ksparse, V_ksparse_H, get_v, H, H_h, cov_x, cov_w, W, num_nodes = greedy_eigenvector_precomputation(
+                    adj, K_sparse, noise, initial_train_mask)
 
-        def get_train_mask_fun():
-            return get_train_mask_greedy(label_percent, initial_train_mask, V_ksparse, V_ksparse_H, get_v, H, H_h,
-                                         cov_x, cov_w, W, num_nodes)
+                def get_train_mask_fun():
+                    return get_train_mask_greedy(label_percent, initial_train_mask, V_ksparse, V_ksparse_H, get_v, H, H_h,
+                                                cov_x, cov_w, W, num_nodes)
 
-        for label_percent in labels_percent_list:
-            result.append(
-                train_and_save(1, label_percent, model_gcn, get_train_mask_fun, adj, features, y_train, y_val, y_test,
-                               initial_train_mask, val_mask, test_mask, True, True, SHOW_TEST_VAL_DATASET_STATS,
-                               VERBOSE_TRAINING, settings, fileinfo, stats_adj_helper))
+                for label_percent in labels_percent_list:
+                    result.append(
+                        train_and_save(1, label_percent, model_gcn, get_train_mask_fun, adj, features, y_train, y_val, y_test,
+                                    initial_train_mask, val_mask, test_mask, True, True, SHOW_TEST_VAL_DATASET_STATS,
+                                    VERBOSE_TRAINING, settings, fileinfo, stats_adj_helper))
 
-        info = {
-            'MAINTAIN_LABEL_BALANCE': 'Greedy',
-            'WITH_TEST': False,
-            'time': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            'params': settings
-        }
+                info = {
+                    'noise': noise,
+                    'K_sparsity': K_sparse,
+                    'time': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    'params': settings
+                }
 
-        dict_output = {'results': result, 'info': info}
-        pk.dump(
-            dict_output,
-            open(
-                os.path.join(result_folder,
-                             fileinfo + 'w_test_features=' + str(False) + '_label_balance=' + 'Greedy' + '_results.p'),
-                'wb'))
-        # else:
-        #     for MAINTAIN_LABEL_BALANCE in maintain_label_balance_list:
-        #         for WITH_TEST in with_test_features_list:
-        #             for label_percent in labels_percent_list:
+                dict_output = {'results': result, 'info': info}
+                pk.dump(
+                    dict_output,
+                    open(
+                        os.path.join(result_folder,
+                                    fileinfo + 'K_sparsity=' + str(K_sparse) + '_label_balance=Greedy_noise='+str(noise) + '_results.p'),
+                        'wb'))
+                # else:
+                #     for MAINTAIN_LABEL_BALANCE in maintain_label_balance_list:
+                #         for WITH_TEST in with_test_features_list:
+                #             for label_percent in labels_percent_list:
 
-        #                 def get_train_mask_fun():
-        #                     return get_train_mask(label_percent, y_train, initial_train_mask, MAINTAIN_LABEL_BALANCE)
+                #                 def get_train_mask_fun():
+                #                     return get_train_mask(label_percent, y_train, initial_train_mask, MAINTAIN_LABEL_BALANCE)
 
-        #                 train_and_save(trials, label_percent, model_gcn, get_train_mask_fun, adj, features, y_train,
-        #                                y_val, y_test, initial_train_mask, val_mask, test_mask, MAINTAIN_LABEL_BALANCE,
-        #                                WITH_TEST, SHOW_TEST_VAL_DATASET_STATS, VERBOSE_TRAINING, settings, fileinfo,
-        #                                stats_adj_helper)
+                #                 train_and_save(trials, label_percent, model_gcn, get_train_mask_fun, adj, features, y_train,
+                #                                y_val, y_test, initial_train_mask, val_mask, test_mask, MAINTAIN_LABEL_BALANCE,
+                #                                WITH_TEST, SHOW_TEST_VAL_DATASET_STATS, VERBOSE_TRAINING, settings, fileinfo,
+                #                                stats_adj_helper)
 
 
 if __name__ == "__main__":
@@ -155,6 +157,7 @@ if __name__ == "__main__":
     features = preprocess_features(features)
 
     labels_percent_list = [5, 10, 15, 20, 30, 40,50, 60,75,85,100]
+    #labels_percent_list = [30,50]
     #list_adj = get_adj_powers(adj.toarray())
     list_adj = None
     maintain_label_balance_list = [True, False]
