bb47a0766a1b9dc48479857b5d533df4cf059c9d
floregol <florence.robert-regol@mail.mcgill.ca>
2019-01-23 14:11:33 -0500

    max degree

--- gcn/sampling/max_degree_sampler.py
+++ gcn/sampling/max_degree_sampler.py
@@ -6,8 +6,11 @@ import numpy as np
 
 
 class MaxDegreeSampler(Sampler):
+
     def __init__(self, initial_train_mask, adj):
         self.name = "MaxDegree"
+        self.initial_train_mask = initial_train_mask
+        self.adj = adj
         self.multi_trials = True
         super(MaxDegreeSampler, self).__init__(initial_train_mask, adj)
 
@@ -18,4 +21,14 @@ class MaxDegreeSampler(Sampler):
 
     def get_train_mask_fun(self, seed):
         np.random.seed(seed=seed)  # To garantee randomness between threads
-        pass
+        degree = np.sum(self.adj, axis=0)
+        train_index = np.argwhere(self.initial_train_mask).reshape(-1)
+        train_mask = np.zeros((initial_train_mask.shape), dtype=bool)  # list of False
+       
+        random_sampling_set_size = int(
+            (label_percent / 100) * train_index.shape[0])
+        random_list = random.sample(
+            range(train_index.shape[0]), random_sampling_set_size)
+        train_mask[random_list] = True
+        label_percent = (100 * np.sum(train_mask) / train_index.shape[0])
+        return train_mask, label_percent
